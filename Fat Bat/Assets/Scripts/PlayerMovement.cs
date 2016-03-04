59717758874fc007f69d9f302b526645171604f4
Morten <mortenhenningjensen@hotmail.com>
2016-03-04 22:38:06 +0100

    Flying bat

---
+++ Fat Bat/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Controls
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    public float rotateSpeed = 4.0f;
+    #endregion
+
+    public CharacterController controller;
+    public CharacterController rightWing;
+    public CharacterController leftWing;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // is the controller on the ground?
+        //  if (controller.isGrounded)
+        //   {
+        //Feed moveDirection with input.
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        //Multiply it by speed.
+        moveDirection *= speed;
+        //Jumping
+        if (Input.GetKey(KeyCode.Q))
+        {
+            moveDirection.y = jumpSpeed;
+
+            //Applying gravity to the controller
+            moveDirection.y -= gravity * Time.deltaTime;
+            //Making the character move
+            rightWing.Move(moveDirection * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            moveDirection.y = jumpSpeed;
+
+            //Applying gravity to the controller
+            moveDirection.y -= gravity * Time.deltaTime;
+            //Making the character move
+            leftWing.Move(moveDirection * Time.deltaTime);
+
+        }
+
+        //   }
+        //Applying gravity to the controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+}
---
+++ Fat Bat/Assets/Scripts/RightWing.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightWing : MonoBehaviour {
+
+    public Rigidbody myRigidbody;
+
+    [SerializeField]
+    private float movementSpeed = 10;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+        }
+
+    }
+}
