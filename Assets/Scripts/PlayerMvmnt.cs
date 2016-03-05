c4ef5158ec07973975258a32ac7d09a56d267394
Nicolai Bülov Nielsen <Nicolai3105@gmail.com>
2016-03-05 16:18:48 +0100

    flappy

--- Assets/Scripts/PlayerMvmnt.cs
+++ Assets/Scripts/PlayerMvmnt.cs
@@ -8,8 +8,13 @@ public class PlayerMvmnt : MonoBehaviour
     //public Rigidbody rightWing;
     // public Rigidbody leftWing;
     private int chargeCD = 10;
+    private bool leftRdy = false;
+    private bool rightRdy = false;
+    private bool wingUpLeft = false;
+    private float friction = 0.95f;
+    private bool wingUpRight = false;
     [SerializeField]
-    private float movementSpeed = 10;
+    private float movementSpeed = 100;
 
     private int points;
 
@@ -18,37 +23,87 @@ public class PlayerMvmnt : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        myRigidbody.mass = 0.1f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float p1Left = Input.GetAxisRaw("Left_P1");
+        float p1Right = Input.GetAxisRaw("Right_P1");
+        
+        //float translate = movementSpeed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Joystick1Button5))
-        {
-            transform.Rotate(0, 0, 5);
-            transform.position += transform.up * Time.deltaTime * movementSpeed;
+        //transform.Rotate(0, 0, Input.GetAxis("Left_P1"));
+        //transform.Rotate(0, 0, -(Input.GetAxis("Right_P1")));
 
-        }
-
-        if (Input.GetKey(KeyCode.E)  || Input.GetKey(KeyCode.Joystick1Button4))
+        if (p1Left <= -0.2)
         {
 
-            transform.Rotate(0, 0, -5);
-            transform.position += transform.up * Time.deltaTime * movementSpeed;
-
+               leftRdy = true;
+               transform.Rotate(0, 0, Input.GetAxis("Left_P1"));
+               //transform.position += transform.up * Time.deltaTime * movementSpeed * 1.5f;              
+            
         }
+        if (p1Left >= 0.2)
+	    {
+		    if (leftRdy == true)
+	        {
+                myRigidbody.AddRelativeForce(Vector3.up * movementSpeed);
+                //transform.position += transform.up * Time.deltaTime * movementSpeed * 1.5f;
+		   
+            leftRdy = false;
+	        }
+            else
+	        {
+                    
+	        }
+	    }
+        if (p1Right <= -0.2)
+        {
 
-        transform.position -= new Vector3(0, 0.1f, 0);
+                rightRdy = true;
+                transform.Rotate(0, 0, -(Input.GetAxis("Right_P1")));
+                //transform.position += transform.up * Time.deltaTime * movementSpeed * 1.5f;
+        }
+        if (p1Right >= 0.2)
+	    {
+		    if (rightRdy == true)
+	        {
+                myRigidbody.AddRelativeForce(Vector3.up * movementSpeed);
+		    
+            //transform.position += transform.up * Time.deltaTime * movementSpeed * 1.5f;
+            rightRdy = false;
+	        }
+            else
+	        {
+                    
+	        }
+	    }
+        //transform.Translate(new Vector3(horizontal, 0, vertical) * translate);
 
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Joystick1Button0))
+        //if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Joystick1Button5))
+        //{
+        //    transform.Rotate(0, 0, 5);
+        //    transform.position += transform.up * Time.deltaTime * movementSpeed;
+
+        //}
+
+        //if (Input.GetKey(KeyCode.E)  || Input.GetKey(KeyCode.Joystick1Button4))
+        //{
+
+        //    transform.Rotate(0, 0, -5);
+        //    transform.position += transform.up * Time.deltaTime * movementSpeed;
+
+        //}
+
+        //DownwardsDraft();
+
+        if (Input.GetKey(KeyCode.Joystick1Button5))
         {
             if (chargeCD >= 10)
             {
-                transform.position += transform.up * Time.deltaTime * 100;
+                myRigidbody.AddRelativeForce(Vector3.up * movementSpeed * 2);
                 chargeCD = 0;
             }
             else
@@ -77,5 +132,9 @@ public class PlayerMvmnt : MonoBehaviour
             cam.transform.position += new Vector3(0, 0, -1);
         }
 
+    }
+    void DownwardsDraft()
+    {
+        transform.position -= new Vector3(0, 0.05f, 0);
     }
 }
