305d1216e990d0a20212a882173aef8e4b7d5f23
Morten <mortenhenningjensen@hotmail.com>
2016-03-05 11:56:05 +0100

    Moving floor

---
+++ Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneMovement : MonoBehaviour
+{
+
+    public Transform[] plane = new Transform[4];
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        plane[0].transform.position += new Vector3(0, 0, -1);
+        plane[1].transform.position += new Vector3(0, 0, -1);
+        plane[2].transform.position += new Vector3(0, 0, -1);
+        plane[3].transform.position += new Vector3(0, 0, -1);
+
+
+
+        if (plane[0].transform.position.z <= -200)
+        {
+            plane[0].transform.position = new Vector3(0, 0f, 1200);
+        }
+
+        if (plane[1].transform.position.z <= -200)
+        {
+            plane[1].transform.position = new Vector3(0, 0f, 1200);
+        }
+
+        if (plane[2].transform.position.z <= -200)
+        {
+            plane[2].transform.position = new Vector3(0, 0f, 1200);
+        }
+
+        if (plane[3].transform.position.z <= -200)
+        {
+            plane[3].transform.position = new Vector3(0, 0f, 1200);
+        }
+
+
+    }
+}
