009426d501ec74a241a70b8201bd568c883f5801
Morten <mortenhenningjensen@hotmail.com>
2016-03-05 12:45:33 +0100

    Planes fixed, removed unused scripts

--- Assets/Scripts/PlaneMovement.cs
+++ Assets/Scripts/PlaneMovement.cs
@@ -15,31 +15,31 @@ public class PlaneMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        plane[0].transform.position += new Vector3(0, 0, -1);
-        plane[1].transform.position += new Vector3(0, 0, -1);
-        plane[2].transform.position += new Vector3(0, 0, -1);
-        plane[3].transform.position += new Vector3(0, 0, -1);
+        plane[0].transform.position += new Vector3(0, 0, -4);
+        plane[1].transform.position += new Vector3(0, 0, -4);
+        plane[2].transform.position += new Vector3(0, 0, -4);
+        plane[3].transform.position += new Vector3(0, 0, -4);
 
 
 
-        if (plane[0].transform.position.z <= -200)
+        if (plane[0].transform.position.z <= -300)
         {
-            plane[0].transform.position = new Vector3(0, 0f, 1200);
+            plane[0].transform.position = new Vector3(12, -17.5f, 1700);
         }
 
-        if (plane[1].transform.position.z <= -200)
+        if (plane[1].transform.position.z <= -300)
         {
-            plane[1].transform.position = new Vector3(0, 0f, 1200);
+            plane[1].transform.position = new Vector3(12, -17.5f, 1700);
         }
 
-        if (plane[2].transform.position.z <= -200)
+        if (plane[2].transform.position.z <= -300)
         {
-            plane[2].transform.position = new Vector3(0, 0f, 1200);
+            plane[2].transform.position = new Vector3(12, -17.5f, 1700);
         }
 
-        if (plane[3].transform.position.z <= -200)
+        if (plane[3].transform.position.z <= -300)
         {
-            plane[3].transform.position = new Vector3(0, 0f, 1200);
+            plane[3].transform.position = new Vector3(12, -17.5f, 1700);
         }
 
 
