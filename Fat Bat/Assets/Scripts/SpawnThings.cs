5ea9ae8f5db744fb57c5e04970f766b4a527e314
Morten <mortenhenningjensen@hotmail.com>
2016-03-05 01:18:49 +0100

    Something Somthing

---
+++ Fat Bat/Assets/Scripts/SpawnThings.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnThings : MonoBehaviour {
+
+    [SerializeField]
+    Rigidbody kittyKat;
+
+    [SerializeField]
+    Transform spawnPos;
+
+    private Vector3 lookPos;
+
+    Vector3 rndSpawnPos;
+
+    Random rnd;
+
+    // Use this for initialization
+    void Start () {
+        // Spawn();
+        // rndSpawnPos = new Vector3(Random.Range(-10, 10), Random.Range(5, 20), Random.Range(30, 50));
+        InvokeRepeating("Spawn", 1, 2);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        rndSpawnPos = new Vector3(Random.Range(-20, 20), Random.Range(5, 20), Random.Range(30, 50));
+       // Spawn();
+
+    }
+
+    void Spawn()
+    {
+        Rigidbody pFab = (Rigidbody)Instantiate(kittyKat, rndSpawnPos, Quaternion.identity);
+
+        pFab.velocity = Quaternion.Euler(lookPos.x, lookPos.y, 0) * (spawnPos.forward * 5);
+
+        Destroy(pFab.gameObject, 10);
+    }
+}
