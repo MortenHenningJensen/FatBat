02add2d02f1e9fd24a0b660f6df1912ca0eb0353
NielsJustesen <niels449@hotmail.com>
2016-03-05 11:57:52 +0100

    DmgSphere, tager points fra playeren og reducerer hans scale

--- Assets/Scripts/PlayerMvmnt.cs
+++ Assets/Scripts/PlayerMvmnt.cs
@@ -63,6 +63,13 @@ public class PlayerMvmnt : MonoBehaviour
             points += 10;
             cam.transform.position += new Vector3(0, 0, -1);
         }
+        if (other.gameObject.CompareTag("DmgPlayer")) 
+        {
+            Destroy(other.gameObject);
+            transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
+            points -= 10;
+            cam.transform.position -= new Vector3(0, 0, 1);
+        }
 
     }
 }
