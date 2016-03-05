67a6b63fcc9814ce21be7635f010aa21edf3d379
Morten <mortenhenningjensen@hotmail.com>
2016-03-05 14:17:04 +0100

    Player one has deathbox

--- Assets/Scripts/PlayerMvmnt.cs
+++ Assets/Scripts/PlayerMvmnt.cs
@@ -11,6 +11,8 @@ public class PlayerMvmnt : MonoBehaviour
     [SerializeField]
     private float movementSpeed = 10;
 
+    public Transform[] colliders = new Transform[3];
+
     private int points;
 
     public Camera cam;
@@ -75,14 +77,34 @@ public class PlayerMvmnt : MonoBehaviour
             transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
             points += 10;
             cam.transform.position += new Vector3(0, 0, -0.5f);
+
+            colliders[0].transform.position += new Vector3(-1, -1, 0);
+            colliders[1].transform.position += new Vector3(1, 1, 0);
+            colliders[2].transform.position += new Vector3(1, 1, 0);
+
+            colliders[0].transform.localScale += new Vector3(0, 5, 0);
+            colliders[1].transform.localScale += new Vector3(0, 5, 0);
+            colliders[2].transform.localScale += new Vector3(5, 0, 0);
+
+
         }
-        if (other.gameObject.CompareTag("DmgPlayer")) 
+        if (other.gameObject.CompareTag("DmgPlayer"))
         {
             Destroy(other.gameObject);
             transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
             points -= 10;
             cam.transform.position -= new Vector3(0, 0, -0.5f);
+
+            colliders[0].transform.position -= new Vector3(0.5f, 0.5f, 0);
+            colliders[1].transform.position -= new Vector3(0.5f, 0.5f, 0);
+            colliders[2].transform.position -= new Vector3(0.5f, 0.5f, 0);
+
         }
 
+        if (other.gameObject.CompareTag("Border"))
+        {
+            Debug.Log("Collision");
+            Destroy(this.gameObject);
+        }
     }
-}
+    }
