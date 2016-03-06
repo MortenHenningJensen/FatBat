bc427f37861fd37e4dde90a030825e4adf8ca4d3
Skinke <mer785@hotmail.com>
2016-03-06 02:42:12 +0100

    not working

--- Assets/FlapScript.cs
+++ Assets/FlapScript.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FlapScript : MonoBehaviour {
+public class FlapScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Vector3 tilt;
+    float tiltRotation = 65.0f;
+    int direction;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1;
+            speed = 5;
+
+        }
+        else
+        {
+            direction = 0;
+            speed = 20;
+
+        }
+
+        //Tilt of the Player in accordance with the movement.
+        tilt = new Vector3(0, 0, direction * tiltRotation);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(tilt), Time.deltaTime * speed);
+    }
 }
